1183d15620388906dfdb49b85cce496717d6e54e
BGarciaQuesada <beleng.quesada@gmail.com>
2026-03-11 10:21:40 +0100

    Comportamientos asociados de funcionalidad, spawn y gravedad

--- Assets/Resources/ZoneScripts/ResourceZone.cs
+++ Assets/Resources/ZoneScripts/ResourceZone.cs
@@ -5,26 +5,46 @@ using UnityEngine;
 
 public class ResourceZone : MonoBehaviour
 {
+    [SerializeField] private GameObject resourcePrefab; // Prefab del recurso que se va a generar al usar la herramienta
+    [SerializeField] private bool canReceiveBuckets;    // Si esta zona puede recibir cubos para rellenarlos
+    [SerializeField] private bool canReceiveTools;      // Si esta zona puede recibir herramientas (pico)
+
     public bool Receive(IItem item, GameObject user)
     {
         // Lo del as: básicamente comprueba de forma segura si el item recibido es del tipo de la variable.
         // Si lo es, se le asigna a la variable
         // Si no, se le asigna null (manejado por el if)
         Bucket bucket = item as Bucket;
-        if (bucket != null && !bucket.IsFull)
+        if (bucket != null && !bucket.IsFull && canReceiveBuckets)
         {
             bucket.Fill();
             return true;
         }
 
         Tool tool = item as Tool;
-        if (tool != null)
+        if (tool != null && canReceiveTools)
         {
             Debug.Log("Herramienta usada en zona");
+
+            // Tiene que spawnear el recurso, y esto tiene que llamar a PlanetController para que se lo añada al inventario del planeta
+            Vector3 spawnPos = transform.position + transform.up * 1.5f;
+            GameObject resource = Instantiate(resourcePrefab, spawnPos, Quaternion.identity);
+
+            ItemGravityController gravity = resource.GetComponent<ItemGravityController>();
+
+            if (gravity != null)
+            {
+                PlayerController player = user.GetComponent<PlayerController>();
+
+                if (player != null)
+                    gravity.currentPlanet = player.currentPlanet;
+            }
+
+
             return true;
         }
 
-        // Si no se reconoce el item (aka no es una herramienta), no se acepta
+        // Si no se reconoce el item (aka no es una herramienta válida), no se acepta
         return false;
     }
 }
---
+++ Assets/Tools/ItemScripts/ItemGravityController.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ItemGravityController : MonoBehaviour
+{
+    public Transform currentPlanet;
+    public float gravity = -20f;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        // Auto-asignar Rigidbody del objeto
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() // Solo ApplyGravity() de player
+    {
+        if (currentPlanet == null) return;
+
+        Vector3 normalVector = (transform.position - currentPlanet.position).normalized;
+
+        rb.AddForce(-normalVector * Mathf.Abs(gravity), ForceMode.Acceleration);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlanetController planet = other.GetComponent<PlanetController>();
+
+        if (planet != null)
+        {
+            currentPlanet = planet.transform;
+        }
+    }
+}

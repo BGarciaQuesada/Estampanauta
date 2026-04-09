8a9b55e31a04e7c32e6e64108015dd44f1513836
Linkthero <amsdrgath@gmail.com>
2026-04-09 11:50:43 +0200

    Objetos funcionan, ya se pueden coger y soltar perfectamente, falta ajustar un poco los tamaños para que sean más grandes y que no desaparezcan en el suelo

--- Assets/Tools/ItemScripts/GrabbableBehavior.cs
+++ Assets/Tools/ItemScripts/GrabbableBehavior.cs
@@ -9,14 +9,23 @@ public class GrabbableBehavior : MonoBehaviour
     [SerializeField] private Transform grabPoint; // Punto donde aparecerá el objeto al ser agarrado
 
     private bool itemEquipped = false;
-    public Collider collider;
+    public Collider colliderColision;   //hay que asignar el objeto Collider hijo
+    private Rigidbody rb;
 
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     public void PickUpitem()
     {
         if (!itemEquipped) 
         {
+            // Avisar al player (busca que tenga PlayerInteraction y, si lo encuentra, le dice que el objeto que tiene en la mano es este)
+            PlayerInteraction player = FindFirstObjectByType<PlayerInteraction>();
+            if (player.objetoEnMano != null)
+                return;
             itemEquipped = true;
-            collider.enabled = false; // Desactivar colisión para evitar problemas al agarrar el objeto
+            colliderColision.enabled = false; // Desactivar colisión para evitar problemas al agarrar el objeto
 
             // Hacer al objeto hijo de la "mano"
             Debug.Log("Me voy a la mano");
@@ -24,14 +33,12 @@ public class GrabbableBehavior : MonoBehaviour
             transform.localPosition = Vector3.zero;
             transform.localRotation = Quaternion.identity;
 
-            // Avisar al player (busca que tenga PlayerInteraction y, si lo encuentra, le dice que el objeto que tiene en la mano es este)
-            PlayerInteraction player = FindFirstObjectByType<PlayerInteraction>();
             
             if (player != null)
             {
                 player.SetHeldItem(gameObject);
-                GetComponent<MeshCollider>().enabled = false; // Desactivar colisión para evitar problemas al agarrar el objeto
-                GetComponent<Rigidbody>().isKinematic = true; // Desactivar física para que el objeto no caiga mientras está agarrado
+                colliderColision.enabled = false; // Desactivar colisión para evitar problemas al agarrar el objeto
+                rb.isKinematic = true; // Desactivar física para que el objeto no caiga mientras está agarrado
                 player.objetoEnMano = gameObject; // Indicar que el jugador tiene un objeto en la mano
             }
 
@@ -50,7 +57,7 @@ public class GrabbableBehavior : MonoBehaviour
     {
         if (itemEquipped)
         {
-            collider.enabled = true; // Reactivar colisión para que el objeto pueda ser agarrado de nuevo
+            colliderColision.enabled = true; // Reactivar colisión para que el objeto pueda ser agarrado de nuevo
             StartCoroutine("CooldownPickUp");
         }
     }
@@ -58,8 +65,8 @@ public class GrabbableBehavior : MonoBehaviour
     IEnumerator CooldownPickUp()
     {
         transform.SetParent(null);
-        GetComponent<MeshCollider>().enabled = true; // Desactivar colisión para evitar problemas al agarrar el objeto
-        GetComponent<Rigidbody>().isKinematic = false; // Desactivar física para que el objeto no caiga mientras está agarrado
+        colliderColision.enabled = true; // Desactivar colisión para evitar problemas al agarrar el objeto
+        rb.isKinematic = false; // Desactivar física para que el objeto no caiga mientras está agarrado
         yield return new WaitForSeconds(2f);
         itemEquipped = false;
         

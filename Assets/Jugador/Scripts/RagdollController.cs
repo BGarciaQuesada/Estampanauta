059aae2e596fc01cd9aa9a6fe218ab12c046ad4e
DavidovisTG <davidvega.3000@gmail.com>
2026-04-22 11:46:21 +0200

    Fixed hit FX

--- Assets/Jugador/Scripts/RagdollController.cs
+++ Assets/Jugador/Scripts/RagdollController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class RagdollControl : MonoBehaviour
 {
     public GameObject root;
-    public Transform pelvis; // Objeto raíz del Ragdoll (puede ser el pelvis o cualquier otro objeto que actúe como raíz)
+    public Transform pelvis; // Objeto raï¿½z del Ragdoll (puede ser el pelvis o cualquier otro objeto que actï¿½e como raï¿½z)
     public Rigidbody[] rigidbodies;
     public Collider[] colliders;
     public Collider mainCollider; // Collider del objeto principal (si es necesario)
@@ -12,6 +12,8 @@ public class RagdollControl : MonoBehaviour
     public Animator animator;
     private PlayerController playerController; // Referencia al script PlayerController para controlar el movimiento del jugador
     private bool quitandoEnergia = false;
+
+    public GameObject sparksFXPrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,25 +27,25 @@ public class RagdollControl : MonoBehaviour
 
     public void ActivaRagdoll()
     {
-        Debug.Log("Activando Ragdoll..."); // Agrega un mensaje de depuración para verificar que se llama a esta función
+        Debug.Log("Activando Ragdoll..."); // Agrega un mensaje de depuraciï¿½n para verificar que se llama a esta funciï¿½n
         animator.enabled = false; // Desactiva el Animator para evitar conflictos con el Ragdoll
         foreach (Rigidbody rb in rigidbodies)
         {
-            rb.isKinematic = false; // Activa la física para cada Rigidbody
+            rb.isKinematic = false; // Activa la fï¿½sica para cada Rigidbody
             rb.AddForce(fuerza, ForceMode.Impulse);
         }
         foreach (Collider col in colliders)
         {
-            col.enabled = true; // Asegura que los Colliders estén habilitados
+            col.enabled = true; // Asegura que los Colliders estï¿½n habilitados
         }
         mainCollider.enabled = false; // Desactiva el Collider del objeto principal para evitar colisiones no deseadas con el Ragdoll
-        playerController.canMove = false; // Desactiva el movimiento del jugador mientras el Ragdoll está activo
+        playerController.canMove = false; // Desactiva el movimiento del jugador mientras el Ragdoll estï¿½ activo
     }
 
     public void DesactivaRagdoll()
     {
-        Debug.Log("Desactivando Ragdoll..."); // Agrega un mensaje de depuración para verificar que se llama a esta función
-                                              // mover root a la posición final del ragdoll
+        Debug.Log("Desactivando Ragdoll..."); // Agrega un mensaje de depuraciï¿½n para verificar que se llama a esta funciï¿½n
+                                              // mover root a la posiciï¿½n final del ragdoll
         transform.position = pelvis.position;
 
         foreach (Rigidbody rb in rigidbodies)
@@ -59,15 +61,15 @@ public class RagdollControl : MonoBehaviour
 
         animator.enabled = true;
         mainCollider.enabled = true; // Reactiva el Collider del objeto principal si es necesario
-        playerController.canMove = true; // Reactiva el movimiento del jugador después de desactivar el Ragdoll
+        playerController.canMove = true; // Reactiva el movimiento del jugador despuï¿½s de desactivar el Ragdoll
     }
 
 
     IEnumerator DesactivaRagdollDespuesDeTiempo(float tiempo)
     {
         yield return new WaitForSeconds(tiempo);
-        DesactivaRagdoll(); // Desactiva el Ragdoll después de un tiempo
-        quitandoEnergia = false; // Permite que se vuelva a quitar energía al colisionar con un enemigo después de desactivar el Ragdoll
+        DesactivaRagdoll(); // Desactiva el Ragdoll despuï¿½s de un tiempo
+        quitandoEnergia = false; // Permite que se vuelva a quitar energï¿½a al colisionar con un enemigo despuï¿½s de desactivar el Ragdoll
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -78,11 +80,16 @@ public class RagdollControl : MonoBehaviour
                 return;
             quitandoEnergia = true;
             Debug.Log("quito 10");
-            GetComponent<Energia>().timer -= 10f; // Resta 10 segundos al tiempo de oxígeno al colisionar con un enemigo
+            GetComponent<Energia>().timer -= 10f; // Resta 10 segundos al tiempo de oxï¿½geno al colisionar con un enemigo
             ActivaRagdoll(); // Activa el Ragdoll al presionar la tecla K
             if (GetComponent<Energia>().timer < 0)
                 return;
-            StartCoroutine(DesactivaRagdollDespuesDeTiempo(2f)); // Desactiva el Ragdoll después de 5 segundos
+            StartCoroutine(DesactivaRagdollDespuesDeTiempo(2f)); // Desactiva el Ragdoll despuï¿½s de 5 segundos
+
+            GameObject fx = Instantiate(sparksFXPrefab, transform.position, Quaternion.identity);
+            fx.transform.SetParent(GetComponent<RagdollControl>().pelvis);
+            fx.transform.localScale = Vector3.one * 2;
+            Destroy(fx, 2F);
         }
             
     }
